e424696220300b66c57d7bf00dc51b22a4173b1a
Yomguithereal <guillaumeplique@gmail.com>
2015-05-07 11:20:59 +0200

    Pimping

--- parse.js
+++ parse.js
@@ -1,20 +1,38 @@
-var rules = require('./rules.js')
+var chalk = require('chalk'),
+    util = require('util'),
+    rules = require('./rules.js'),
+    _ = require('lodash');
 
 var amendements = require('./data/renseignement.json').amendements;
 
 // Temporary
 function preprocess(txt) {
-  return txt.replace(/\s/g, ' ');
+  return txt.replace(/\s/g, ' ').replace(/(<p>|<\/p>)/g, '');
 }
 
+var matches = 0;
+
 amendements.forEach(function(row, i){
+  var amendement = preprocess(row.amendement.texte);
 
-  var result = rules.parse(preprocess(row.amendement.texte));
+  var result = rules.parse(preprocess(amendement));
 
-  // Adding original amendement for reference and debugging
-  if (result)
-    result.amendement = row.amendement.texte;
+  // Formatting output
+  var output = '';
 
-  console.log(result);
-})
+  if (!result) {
+    output = chalk.red('No match for:') + ' ' + _.trunc(amendement, 100);
+  }
+  else {
+    matches++;
+    output = chalk.green('Match for:') + ' ' + _.trunc(amendement, 100);
+    output += '\n' + util.inspect(result);
+  }
 
+  output += '\n';
+
+  console.log(output);
+});
+
+// Outputting report
+console.log(chalk.blue('Report: ') + matches + '/' + amendements.length + ' amendements.\n');
